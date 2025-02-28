5a106098755e1fd9cfedf52b85367881f8fc1600
Yi-Steven-Zhou <yi.zhou@stu.ocsb.ca>
2025-02-28 17:53:05 +0000

    Emre Added his page for fixingin it

--- index.php
+++ index.php
@@ -1,57 +1,37 @@
 <!DOCTYPE html>
-<!-- ICS2O-Unit1-07-HTML-MDL-PHP -->
-<html lang="en-ca">
+  <html>
   <head>
-    <meta charset="utf-8" />
-    <meta name="description" content="Hello, World!, PHP" />
-    <meta name="keywords" content="mths, icd2o" />
-    <meta name="author" content="Mr. Coxall" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link
-      rel="stylesheet"
-      href="https://fonts.googleapis.com/icon?family=Material+Icons"
-    />
-    <link
-      rel="stylesheet"
-      href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css"
-    />
-    <link rel="stylesheet" href="./css/style.css" />
-    <link
-      rel="apple-touch-icon"
-      sizes="180x180"
-      href="./apple-touch-icon.png"
-    />
-    <link
-      rel="icon"
-      type="image/png"
-      sizes="32x32"
-      href="./favicon-32x32.png"
-    />
-    <link
-      rel="icon"
-      type="image/png"
-      sizes="16x16"
-      href="./favicon-16x16.png"
-    />
-    <link rel="manifest" href="./site.webmanifest" />
-    <title>Hello, World!, PHP</title>
-  </head>
-
+  <meta charset="utf-8" />
+  <meta name="description" content="My first webpage, with a favicon" />
+  <meta name="keywords" content="mths, icd2o" />
+  <meta name="author" content="Emre Guzel" />
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.blue_grey-light_green.min.css" />
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Atkinson+Hyperlegible+Mono:ital,wght@0,200..800;1,200..800&display=swap" rel="stylesheet">
+  <link rel="apple-touch-icon" sizes="180x180" href="./apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="./favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="./favicon-16x16.png">
+  <link rel="manifest" href="/site.webmanifest">
+  <link rel="stylesheet" href="css/style.css">
+</head>
+  <title>Php</title>
   <body>
     <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
-    <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
-      <header class="mdl-layout__header">
-        <div class="mdl-layout__header-row">
-          <span class="mdl-layout-title">Hello, World!, PHP</span>
+  <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+    <header class="mdl-layout__header">
+      <div class="mdl-layout__header-row">
+        <span class="mdl-layout-title">PHP</span>
+      </div>
+    </header>
+    <img class="PHP" src="images/PHP.jpg" alt="">
+    <br>
+    <main id="click me " class="mdl-layout__content">
+      <div class="page-content">
+        <?php echo '<p>Hello, World!, in PHP</p>'; ?>
         </div>
-      </header>
-      <img src="./images/php.jpg" />
-      <main class="mdl-layout__content">
-        <div class="page-content">
-          <?php echo '<p>Hello, World!, in PHP</p>'; ?>
-        </div>
-        <!-- Simple Textfield for integers-->
-      </main>
-    </div>
+    </main>
+  </div>
   </body>
 </html>
